1fc4bc9f28b8fd67de22482cfd641633e4e52330
jyx <jiaoyuxin@mioji.com>
2018-09-29 18:14:04 +0800

    no message

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ const lvRouter = require('./lvRouter.js');
 
 const router = new Router();
 router.use('/lvyouju', lvRouter.routes(), lvRouter.allowedMethods());
+router.post('/wx', async (ctx) => {
+  console.log(11111);
+  ctx.body = {}
+});
 router.get('/', async (ctx) => {
   await ctx.render('index');
 });
