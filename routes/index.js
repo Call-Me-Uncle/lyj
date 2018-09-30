e1f724a403aea789bda60d6234c181095ed5a4b6
jyx <jiaoyuxin@mioji.com>
2018-09-30 10:26:46 +0800

    更新返回

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const lvRouter = require('./lvRouter.js');
 const router = new Router();
 router.use('/lvyouju', lvRouter.routes(), lvRouter.allowedMethods());
 router.post('/wx', async (ctx) => {
-  console.log(11111);
-  ctx.body = {}
+  console.log(ctx.request.body);
+  ctx.body = ctx.request.body;
 });
 router.get('/', async (ctx) => {
   await ctx.render('index');
 });
 module.exports = router;
+
+
